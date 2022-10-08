45e14ff73094033bba8e1ab952741abf54395bef
drharrington <drharrington@csu.fullerton.edu>
2022-10-07 17:11:06 -0700

    update 3
    created new incomplete navbar

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,12 @@
 <script>
-  import devLogo from './assets/dev-logo.svg'
-  import Counter from './lib/Counter.svelte'
-  import Navbar from './lib/Navbar.svelte';
+  import NavBar from "./lib/NavBar.svelte";
 </script>
 
 <main>
-  <div>
-    <a href="https://acmcsuf.com" target="_blank"> 
-      <img src={devLogo} class="logo dev" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Dominic Harrington</h1>
+  <NavBar/>
 
-  <div class="card">
-    <Counter />
-    <Navbar />
-  </div>
 </main>
 
 <style>
-  .logo {
-    height: em;
-    padding: 1.5em;
-    will-change: filter;
-  }
   
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-
-  .logo.dev:hover {
-    filter: drop-shadow(0 0 2em #1E6CFF);
-  }
 </style>
